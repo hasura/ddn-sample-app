63f0a994e4bdcf5d39c443be11333a201d03c867
Rahul Agarwal <111393043+rahulagarwal13@users.noreply.github.com>
2024-04-17 14:44:36 -0700

    desc, CH update, deprecation

--- sales/sales_functions/connector/functions.ts
+++ sales/sales_functions/connector/functions.ts
@@ -55,7 +55,7 @@ interface GithubProfile {
 }
 
 /**
- * Returns the github bio for the userid provided
+ * Returns the github bio for the userid provided with batching
  *
  * @param username Username of the user who's bio will be fetched.
  * @returns The github bio for the requested user.
@@ -87,7 +87,7 @@ export async function get_github_profile_description(
 }
 
 /**
- * Returns the github bio for the userid provided
+ * Returns the github bio for the userid provided without batching
  *
  * @param username Username of the user who's bio will be fetched.
  * @returns The github bio for the requested user.
@@ -96,6 +96,35 @@ export async function get_github_profile_description(
  */
 
  export async function get_github_profile_description_noparallel(
+  username: string
+): Promise<string | null> {
+  try {
+    // const response = await fetch(`https://api.github.com/users/${username}`);
+    const response = await fetch(`https://httpbin.org/delay/3`);
+
+    // Check if the response is successful
+    if (!response.ok) {
+      throw new Error(`Failed to fetch GitHub profile: ${response.statusText}`);
+    }
+
+    // Use type assertion to tell TypeScript the expected type of the JSON response
+    const data = (await response.json()) as GithubProfile;
+
+    // Handle the case where 'bio' is null or undefined
+    return data.bio ?? "No bio available.";
+  } catch (error) {
+    console.error("Error fetching GitHub profile:", error);
+    throw error; // Or handle it as appropriate for your application
+  }
+}
+
+/**
+ * Typescript Function with a 300 HTTP Delay
+ * @readonly This function should only query data without making modifications
+ * @paralleldegree 5
+ */
+
+export async function http300delay(
   username: string
 ): Promise<string | null> {
   try {
