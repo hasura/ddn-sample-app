70ff3c35ece00694838be490fbe9f1cefb66ca96
Sooraj Sanker <soorajshankar@gmail.com>
2024-06-19 14:11:51 +0530

    users on cloud

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,12 @@ ddn project subgraph create sales
 ddn project subgraph create experience
 ddn project subgraph create users
 
-## deploy connectors from sales subgraph
+## deploy connectors from analytics subgraph
 ddn connector build create --connector analytics/connector/clickhouse/connector.cloud.yaml --target-supergraph supergraph.cloud.yaml --target-connector-link clickhouse
 
 ## deploy connectors from sales subgraph
 ddn connector build create --connector sales/connector/pg/connector.cloud.yaml --target-supergraph supergraph.cloud.yaml --target-connector-link pg
 ddn connector build create --connector sales/connector/ts/connector.cloud.yaml --target-supergraph supergraph.cloud.yaml --target-connector-link ts --log-level DEBUG
+
+## deploy connectors from users subgraph
+ddn connector build create --connector users/connector/user_pg/connector.cloud.yaml --target-supergraph supergraph.cloud.yaml --target-connector-link user_pg
